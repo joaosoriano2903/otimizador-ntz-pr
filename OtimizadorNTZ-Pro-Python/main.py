89c8cf15c4fb0ff99f06da141476e875ea26b015
JoÃ£o Pedro Soriano <seuemail@gmail.com>
2025-04-15 13:19:28 -0300

    📌 Auto: Alterado main.py

---
+++ OtimizadorNTZ-Pro-Python/main.py
@@ -0,0 +1,10 @@
+from gui import menu_principal
+
+if __name__ == "__main__":
+    try:
+        print("Iniciando a interface do System Optimizer...")
+        menu_principal.iniciar_interface()
+    except Exception as e:
+        print(f"Ocorreu um erro inesperado: {e}")
+    finally:
+        print("Encerrando o programa. Até logo!")
